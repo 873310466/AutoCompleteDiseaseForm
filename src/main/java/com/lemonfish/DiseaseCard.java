81f7b1e0131e3b4e8d7c38bc9985c5046307f832
lemonfishzc <5705784+lemonfishzc@user.noreply.gitee.com>
2020-04-17 11:20:36 +0800

    完美适应新平台~

--- src/main/java/com/lemonfish/DiseaseCard.java
+++ src/main/java/com/lemonfish/DiseaseCard.java
@@ -1,6 +1,6 @@
 package com.lemonfish;
-import	java.util.Random;
-import	java.util.Scanner;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,34 +15,43 @@ import org.openqa.selenium.support.ui.Select;
  * @date 2020/3/26 16:34
  */
 public class DiseaseCard {
-    public static void main(String[] args) throws InterruptedException {
 
+    public static final String USERNAME = "20180824";
+    public static final String PASSWORD = "209304";
+    public static final String QUERY_URL = "https://www.wjx.cn/jq/61862730.aspx";
+    public static final String NAME = "张超";
+    public static final String WEBDRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
+
+    public static void main(String[] args) throws InterruptedException {
+//209304
         Scanner in = new Scanner(System.in);
+        Map<String, String> map = new HashMap<>();
         // 需要填写的信息
-        System.out.println("请输入你的名字");
-        String name = in.next();
+//        System.out.println("请输入你的名字");
+//        String name = in.next();
         System.out.println("请输入你的体温(36.X)的X，即一位");
         String temperature = in.next();
-        System.out.println("请输入你云成长的帐号");
-        String username = in.next();
-        System.out.println("请输入你云成长的密码");
-        String password = in.next();
-        System.out.println("因为每个班级的问卷调查网址不同，请输入问卷调查完整网址（建议复制）:\n\r(软件1809班同学输入 9 即可)");
-        String url = in.next();
-        if ("9".equals(url)) {
-            url = "https://www.wjx.cn/jq/61862730.aspx";
-        }
-        autoCompleteForm(name, temperature, username, password, url);
+//        System.out.println("请输入你云成长的帐号");
+//        String username = in.next();
+//        System.out.println("请输入你云成长的密码");
+//        String password = in.next();
+//        System.out.println("因为每个班级的问卷调查网址不同，请输入问卷调查完整网址（建议复制）:\n\r(软件1809班同学输入 9 即可)");
+//        String url = in.next();
+//        if ("9".equals(url)) {
+//            url = ;
+//        }
+        autoCompleteForm(NAME, temperature, USERNAME, PASSWORD, QUERY_URL);
 
     }
 
     private static void autoCompleteForm(String name, String temperature, String username, String password, String url) throws InterruptedException {
         // 设置webdriver路径
-        System.setProperty("webdriver.chrome.driver", DiseaseCard.class.getClassLoader().getResource("chromedriver.exe").getPath());
+        System.setProperty(WEBDRIVER_CHROME_DRIVER, DiseaseCard.class.getClassLoader().getResource("chromedriver.exe").getPath());
+
         WebDriver webDriver = new ChromeDriver();
-
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         webDriver.get(url);
-
+        HashMap<String, String> map = new HashMap<>();
 
         // 问卷调查
         webDriver.findElement(By.xpath("//*[@id=\"divquestion1\"]/ul/li[1]/a")).click();
@@ -58,9 +67,9 @@ public class DiseaseCard {
         webDriver.switchTo().defaultContent();
         webDriver.findElement(By.xpath("//*[@id=\"divquestion5\"]/ul/li[1]/a")).click();
         webDriver.findElement(By.xpath("//*[@id=\"divquestion7\"]/ul/li[5]/a\n")).click();
-        // 停止15-20S，模拟自己填写的情况
+//         停止15-20S，模拟自己填写的情况
         Random rand = new Random();
-        long doneTime = rand.nextInt(5000)+15000;
+        long doneTime = rand.nextInt(2000)+10000;
         Thread.sleep(doneTime);
         webDriver.findElement(By.className("submitbutton")).click();
 
@@ -68,23 +77,25 @@ public class DiseaseCard {
 
 
         // 云成长
-        webDriver.get("http://stuinfo.neu.edu.cn/#/");
+        /*webDriver.get("http://stuinfo.neu.edu.cn/#/");
         webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div[1]/div[3]/div[2]/div[1]/input")).sendKeys(username);
         webDriver.findElement(By.xpath("//*[@id=\"app\"]/div/div[1]/div[3]/div[2]/div[2]/div/input")).sendKeys(password);
         webDriver.findElement(By.className("loginButton")).click();
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         webDriver.findElement(By.xpath("//*[@id=\"app\"]/section/aside/div/ul/li[2]")).click();
         Thread.sleep(1000);
         webDriver.findElement(By.xpath("//*[@id=\"app\"]/section/section/section/section/main/div[1]/div[1]/div/div[2]/div/div/div")).click();
         Thread.sleep(2000);
         String originWindow = webDriver.getWindowHandle();
         for(String winHandle : webDriver.getWindowHandles()) {
+            System.out.println(winHandle);
             if (winHandle.equals(originWindow)) {
                 continue;
             }
             webDriver.switchTo().window(winHandle);
             break;
         }
+        Thread.sleep(3000);
         WebElement select = webDriver.findElement(By.id("sfgcyiqz"));
         Select select1 = new Select(select);
         select1.selectByValue("否");
@@ -101,6 +112,7 @@ public class DiseaseCard {
                 select2.selectByValue("无");
             }
         }
-        webDriver.findElement(By.xpath("/html/body/div/div/div/button")).click();
+        Thread.sleep(3000);
+        webDriver.findElement(By.xpath("/html/body/div/div/div/button")).click();*/
     }
 }
---
+++ src/main/java/com/lemonfish/NewDiseaseCard.java
@@ -0,0 +1,94 @@
+package com.lemonfish;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Chao Zhang
+ * @version V1.0 安全版
+ * @Package com.lemonfish.disease
+ * @date 2020/3/26 16:34
+ */
+public class NewDiseaseCard {
+
+    public static final String USERNAME = "20180824";
+    public static final String PASSWORD = "123456as";
+    public static final String QUERY_URL = "https://www.wjx.cn/jq/61862730.aspx";
+    public static final String NAME = "张超";
+    public static final String WEBDRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
+
+    public static void main(String[] args) throws InterruptedException {
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("请输入你的体温(36.X)的X，即一位");
+//        String temperature = in.next();
+
+        autoCompleteForm(NAME, "0", USERNAME, PASSWORD, QUERY_URL);
+
+    }
+
+    private static void autoCompleteForm(String name, String temperature, String username, String password, String url) throws InterruptedException {
+        // 设置webdriver路径
+        System.setProperty(WEBDRIVER_CHROME_DRIVER, NewDiseaseCard.class.getClassLoader().getResource("chromedriver.exe").getPath());
+
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        webDriver.get(url);
+
+        // 问卷调查
+        webDriver.findElement(By.xpath("//*[@id=\"divquestion1\"]/ul/li[1]/a")).click();
+        webDriver.findElement(By.id("q2")).click();
+        webDriver.switchTo().frame("PDF_i_chezchenz");
+        webDriver.findElement(By.className("search-txt")).sendKeys(name);
+        webDriver.findElement(By.className("item2")).click();
+        webDriver.switchTo().defaultContent();
+        webDriver.findElement(By.className("underline")).sendKeys(temperature);
+        webDriver.findElement(By.id("q4")).click();
+        webDriver.switchTo().frame("__calendarIframe");
+        webDriver.findElement(By.id("selectTodayButton")).click();
+        webDriver.switchTo().defaultContent();
+        webDriver.findElement(By.xpath("//*[@id=\"divquestion5\"]/ul/li[1]/a")).click();
+        webDriver.findElement(By.xpath("//*[@id=\"divquestion7\"]/ul/li[5]/a\n")).click();
+//         停止8-12S，模拟自己填写的情况
+        Random rand = new Random();
+        long doneTime = rand.nextInt(4000)+8000;
+        Thread.sleep(doneTime);
+        webDriver.findElement(By.className("submitbutton")).click();
+
+        Thread.sleep(1000);
+
+
+        // 师生健康信息上报平台
+        webDriver.get("https://ehall.neu.edu.cn/db_portal/guide?id=D06BDA87-2E6E-4324-A14D-3BFAD839F2B9");
+
+        webDriver.findElement(By.className("btn-blue")).click();
+
+        // 跳转新Tab
+        String originWindow = webDriver.getWindowHandle();
+        for(String winHandle : webDriver.getWindowHandles()) {
+            if (winHandle.equals(originWindow)) {
+                continue;
+            }
+            webDriver.switchTo().window(winHandle);
+            break;
+        }
+
+        webDriver.findElement(By.id("un")).sendKeys(USERNAME);
+        webDriver.findElement(By.id("pd")).sendKeys(PASSWORD);
+        webDriver.findElement(By.id("index_login_btn")).click();
+
+        webDriver.findElement(By.xpath("//*[@id=\"app\"]/main/div/form/div[1]/table/tbody/tr/td[1]/div/div/div/label[1]/span[1]/span")).click();
+        webDriver.findElement(By.xpath("//*[@id=\"app\"]/main/div/form/div[3]/div[2]/table/tbody/tr[1]/td/div/div/div/label[1]/span[1]/span")).click();
+        Thread.sleep(1000);
+        webDriver.findElement(By.xpath("//*[@id=\"app\"]/main/div/form/div[4]/div[2]/table/tbody/tr[1]/td/div/div/div/label[1]/span[1]/span")).click();
+        webDriver.findElement(By.xpath("//*[@id=\"app\"]/main/div/form/div[5]/div[2]/div/div/div/textarea")).sendKeys("大家辛苦啦 !!!");
+        Thread.sleep(1000);
+        webDriver.findElement(By.xpath("//*[@id=\"app\"]/main/div/form/div[6]/button")).click();
+    }
+}
